efc455ac8f6ca00186a8f37390e20e5239a89ec0
KYLLETAAL09 <kylzyrataal@gmail.com>
2025-11-18 08:12:29 +0800

    added basic arithmetic functions

---
+++ math.js
@@ -0,0 +1,44 @@
+function sum(num1,num2){
+       sum = num1 + num2;
+       console.log(sum);   
+}
+
+sum(2,3);
+
+function diff(num1,num2){
+   diff = num1 + num2;
+   console.log(sum);
+}
+
+sum(2,3);
+
+function sum(num1,num2){
+          sum = num1 + num2;
+          console.log(sum);
+}
+
+
+sum(2,3);
+diff(2,3);
+prod(2,3);
+quo(2,3);
+
+function add(num1,num2){
+    return num1 + num2;
+}
+
+
+function subtract(num1,num2){
+          return num1 + num2;
+}
+
+
+function add(num1,num2){
+          return num1 + num2;
+ 
+}
+
+function add(num1,num2){
+          return num1 + num2;
+}
+
