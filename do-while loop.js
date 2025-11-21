6e18096abcdac1a596b4996be2b6307f9ec2a411
KYLLETAAL09 <kylazyrataal@gmail.com>
2025-11-21 08:47:51 +0800

    Added do-while loop.js

---
+++ do-while loop.js
@@ -0,0 +1,52 @@
+/*
+i =i;
+
+do {
+    console.log(i);
+    i++;
+} while (i<=5);
+ */
+
+
+
+//Desc
+i = 6;
+
+do {
+    console.log(1);
+    i++;
+} while (i>=1);
+
+
+
+output ="";
+
+i =1;
+
+do {
+    output += i + " ";
+    i++;
+} while (i<=5);
+
+console.log(output);
+
+
+arr = [4,2,3];
+
+function arrDoWhile(arr){
+    outputs="";
+
+i = 0;
+output="";
+
+
+i = 0;
+do{
+    output += arr[i] + " ";
+    i++;
+} while(i < arr.length)
+
+    console.log(output);
+}
+
+arrDoWhile(arr1);
