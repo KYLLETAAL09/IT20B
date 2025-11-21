660215a6decf92d8816188f7ee4869eb9278937e
KYLLETAAL09 <kylazyrataal@gmail.com>
2025-11-21 08:31:19 +0800

    Adedd while-loop.js

---
+++ while-loop.js
@@ -0,0 +1,56 @@
+/*
+i = 5;
+
+//desc
+
+while (i>=1){
+    console.assert.log(i);
+    i--;
+}
+
+    */
+   //online Desc
+/*
+i =1;
+
+while (i<=5){
+    console.log(i);
+    i++;
+}
+
+output ="*";
+i =i;
+
+while (i<=5){
+    output += i + i + " ";
+    i++;
+}
+
+console.log(output);
+
+
+
+
+
+
+//online Desc 
+output = "";
+i =5;
+
+while (i>=1){
+    output += i + " ";
+    1--;
+}
+
+console.log(output);
+
+*/
+arr=[4,2,3];
+output = " ";
+i=0;
+
+while(i<+arr.length-1){
+    output += arr[i] + " ";
+}
+
+console.log(output);
