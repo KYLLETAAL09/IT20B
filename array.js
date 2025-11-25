fe4768795c7192bf2e6c2cc80609020a389ce3eb
KYLLETAAL09 <kylazyrataal@gmail.com>
2025-11-25 08:15:49 +0800

    Added delete array method

--- array.js
+++ array.js
@@ -37,10 +37,30 @@ class array{
         this.data[index] = element;
         this.length++;
     }
+
+    delete(index){
+
+      //correcting statement || Must be False
+      if(index <0 || index >= this.length){
+        console.log("Invalid Index");
+        return;
+      }  
+
+      //Shifting Condition
+      for(let i= index; i< this.length; i++){
+        this.data[i] = this.data[i=1];
+      }
+
+      //After Condition
+      delete this.data[this.length-1];
+      this.length--;
+    }
 }
 
 //
 arr1 = new Array([4,2,3]);
 arr1.traverse();
 arr1.insert(0,9);
+arr1.traverse();
+arr1.delete(2);
 arr1.traverse();
