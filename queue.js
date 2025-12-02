80beabadec4ff393bd504fbfd9cc56ca705b45e0
KYLLETAAL09 <kylazyrataal@gmail.com>
2025-12-02 09:24:27 +0800

    Added search method for DSA/linkedlist

---
+++ queue.js
@@ -0,0 +1,20 @@
+class Queue{
+    constructor(){
+        this.data = {};
+        this.frontIndex = 0;
+        this.rearIndex = -1;
+    }
+    
+    enqueue(element){
+        this.rearIndex++;
+        this.data[this.rearIndex] = element;
+
+    }
+    traverse(){
+        let output = "";
+        for(let i=this.frontIndex; i<= this.rearIndex; i++){
+            output += this.data[i] + " ";
+        }
+    }
+        return output
+}   
