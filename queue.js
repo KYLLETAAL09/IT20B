87848fcd831ff73033e8821d09a316bd88a3e351
KYLLETAAL09 <kylazyrataal@gmail.com>
2025-12-02 09:45:32 +0800

    Added initial classes and methods for DSA/linkedlist

--- queue.js
+++ queue.js
@@ -31,12 +31,22 @@ class Queue{
 
     size(){
         return this.rearIndex - this.frontIndex +1;
+         }
+
+    dequeue(){
+        //Correction Statement
+        if(this.is_empty()) return null;
+
+        const value = this.data[this.frontIndex];
+        delete this.data[this.frontIndex];
+        this.frontIndex++;
+        return value;
     }
 }
-   
+
 
 //
-let queue1 = new Queue();
+@@ -44,5 +54,6 @@ queue1.enqueue(2);
 console.log(queue1.is_empty());
 queue1.enqueue(4);
 console.log("The current queue size is: " + queue1.size());
@@ -47,5 +57,3 @@ console.log(queue1.traverse());
 console.log("The current queue size is: " + queue1.size());
 console.log(queue1.is_empty());
 console.log(queue1.front());
-
-
